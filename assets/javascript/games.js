99d6b52a8e7068e136f4df3632866a2c0d6d7212
DasBarenz <42988309+DasBarenz@users.noreply.github.com>
2018-10-06 21:14:06 -0600

    pretty close, except the game is not resetting

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -3,10 +3,10 @@ var wins = 0;
 var losses = 0;
 var guessesRemain = 8;
 var secretLetter = "";
-var guesses = "";
+var guesses = [];
 var userGuess = "";
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //computer selects random letter at the start of every game
 var secretLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
@@ -16,51 +16,34 @@ console.log(secretLetter);
 document.onkeyup = function(event) {
 
 // Determines which key was pressed.
-var userGuess = event.key;
-// console.log(userGuess);
-userGuess.html("");
+    var userGuess = event.key;
+    console.log(userGuess);
 
-if (userGuess === secretLetter) {
-    wins++;
-    guessesRemain = 8;
-    guesses = "";
-    var secretLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-} else {
-    guessesRemain--;
-} 
+// adds the user guess to an array (for later display)
+    guesses.push(" " + userGuess);
+    console.log(guesses);
 
-if (guessesRemain === 0) {
-    losses++;
-    guessesRemain = 8;
-    guesses = "";
-    var secretLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-}
+//if correct guess, you win and evverything resets...if you don't get the correct guess, the remaining guesses go down by one
+    if (userGuess == secretLetter) {
+        wins++;
+        guessesRemain = 8;
+        guesses = [];
+        secretLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    } else {
+        guessesRemain--;
+    } 
 
-guesses.append("userGuess" + ", ");
-// Randomly chooses a choice from the options array. This is the Computer's guess.
-// var secretLetter = alphabet[Math.random(Math.floor * alphabet.length)];
-// console.log(secretLetter);
+//if you reach 0 guesses remaining, the losses increase by one and the the game resets
+    if (guessesRemain === 0) {
+        losses++;
+        guessesRemain = 8;
+        guesses = [];
+        secretLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+//update wins, losses, guesses, guesses remaining onto the html
+    document.getElementById("winScore").textContent = "Wins: " + wins;
+    document.getElementById("lossScore").textContentL= "Losses: " + losses;
+    document.getElementById("guessRemain").textContent= "Guesses remaining: " + guessesRemain;
+    document.getElementById("guesses").textContent= guesses;
 
 };
-// var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  
-    //     // Alerts the key the user pressed (userGuess).
-    //     alert("User guess: " + userGuess);
-  
-    //     // Alerts the Computer's guess.
-    //     alert("Computer guess: " + computerGuess);
-
-
-//computer generates secretLetter from var alphabet-->
-
-
-// wins start at zero and increment up 1 with each win-->
-
-
-//losses start at zero and increment up 1 with each loss -->
-
-
-//guessesRemain start at 8 and count down with each letter guessed, when it reaches zero, the loss count increases by one -->
-
-
-//this is a list (ARRAY?) of letters that the player has guessed, separated by commas -->
